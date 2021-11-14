abeb26bd3343064b5af4f4c44c21056b6d5a18ce
Eduardo Melo <92065918+eduardoddmg@users.noreply.github.com>
2021-11-14 00:00:34 -0300

    Add files via upload

---
+++ app.js
@@ -0,0 +1,51 @@
+function button1 (){
+    var img = document.getElementById('img');
+    var txt1 = document.getElementById('txt1');
+    var txt2 = document.getElementById('txt2');
+    var btn1 = document.getElementById('btn-1');
+    var btn2 = document.getElementById('btn-2');
+    var btn3 = document.getElementById('btn-3');
+
+    img.src = "img1.jpg"
+    txt1.innerHTML = "Maria";
+    txt2.innerHTML = "Web Designer"
+    btn1.style.backgroundColor = "black";
+    btn2.style.backgroundColor = "white";
+    btn3.style.backgroundColor = "white";
+
+}
+
+function button2 (){
+    var img = document.getElementById('img');
+    var txt1 = document.getElementById('txt1');
+    var txt2 = document.getElementById('txt2');
+    var btn1 = document.getElementById('btn-1');
+    var btn2 = document.getElementById('btn-2');
+    var btn3 = document.getElementById('btn-3');
+
+    img.src = "img2.jpg"
+    txt1.innerHTML = "Joana";
+    txt2.innerHTML = "Web Developer"
+    btn1.style.backgroundColor = "white";
+    btn2.style.backgroundColor = "black";
+    btn3.style.backgroundColor = "white";
+
+}
+
+function button3 (){
+    var img = document.getElementById('img');
+    var txt1 = document.getElementById('txt1');
+    var txt2 = document.getElementById('txt2');
+    var btn1 = document.getElementById('btn-1');
+    var btn2 = document.getElementById('btn-2');
+    var btn3 = document.getElementById('btn-3');
+
+    img.src = "img3.jpg"
+    txt1.innerHTML = "Joao";
+    txt2.innerHTML = "Social Media"
+    btn1.style.backgroundColor = "white";
+    btn2.style.backgroundColor = "white";
+    btn3.style.backgroundColor = "black";
+}
+
+
